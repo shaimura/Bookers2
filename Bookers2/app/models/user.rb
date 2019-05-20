05615379f5be12edcb6ed0f3357823e30fbd0133
志村隼斗 <daken@shimurahayatonoMacBook-Air.local>
2019-05-20 18:43:45 +0900

    [update]Bookページ追加

--- Bookers2/app/models/user.rb
+++ Bookers2/app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
 
   has_many :books, dependent: :destroy
+  attachment :image
 
 end
