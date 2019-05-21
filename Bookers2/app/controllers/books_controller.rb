c7f81bfb813b868cdbf9f3723fdb53cf716b1b13
志村隼斗 <daken@shimurahayatonoMacBook-Air.local>
2019-05-21 15:28:11 +0900

    [add]Book詳細ページ追加

--- Bookers2/app/controllers/books_controller.rb
+++ Bookers2/app/controllers/books_controller.rb
@@ -3,7 +3,6 @@ class BooksController < ApplicationController
 	def index
 		@books = Book.all
 		@book = Book.new
-		@user = current_user.id
 	end
 
 
@@ -11,20 +10,33 @@ class BooksController < ApplicationController
 		@book = Book.new(book_params)
 		@book.user_id = current_user.id
 		@book.save
-		redirect_to books_path
+		redirect_to book_path(@book.id)
 	end
 
 	def show
 		@book = Book.find(params[:id])
+		@new = Book.new
+		@user = @book.user
+	end
+
+	def edit
+		@book = Book.find(params[:id])
+	end
+
+	def update
+		@book = Book.find(params[:id])
+		@book.update(book_params)
+		redirect_to book_path(@book)
 	end
 
 	def destroy
 		@book = Book.find(params[:id])
 		@book.destroy
-		redirect_to books_path
+		redirect_to user_path(@book.user)
 	end
 
 	private
+
 	def book_params
 		params.require(:book).permit(:title, :body)
 	end
--- Bookers2/config/routes.rb
+++ Bookers2/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only:[:new, :create, :index, :show, :update, :destroy, :top]
+  resources :books, only:[:new, :create, :index, :show, :update, :destroy, :top, :edit]
   resources :users, only:[:show, :index, :edit, :update]
 
 root to: 'books#index'
