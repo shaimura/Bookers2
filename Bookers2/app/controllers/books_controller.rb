e52835e1a63549f7a2b1fde0377f3530000bd52e
志村隼斗 <daken@shimurahayatonoMacBook-Air.local>
2019-05-23 03:07:14 +0900

    [add]編集ページのURL直打ち防止

--- Bookers2/app/controllers/books_controller.rb
+++ Bookers2/app/controllers/books_controller.rb
@@ -1,22 +1,30 @@
 class BooksController < ApplicationController
 	before_action :authenticate_user!
+	before_action :correct_user, only: [:edit, :update]
+
+    def correct_user
+        redirect_to root_url
+    end
 
 	def index
 		@books = Book.all
+		@book = Book.new
 	end
 
 
 	def create
+		@books = Book.all
 		@book = Book.new(book_params)
 		@book.user_id = current_user.id
 		if @book.save
 		   redirect_to book_path(@book.id)
 		else
-			redirect_to books_path , flash: {error: '入力エラー'}
+			render :index
 		end
 	end
 
 	def show
+		@books = Book.all
 		@book = Book.find(params[:id])
 		@user = @book.user
 	end
--- Bookers2/app/controllers/users_controller.rb
+++ Bookers2/app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update]
+
+
+  def correct_user
+    redirect_to root_url
+  end
 
   def show
   	@user = User.find(params[:id])
