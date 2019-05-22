db711c04c659a6da337de26da745c8025e337f0c
志村隼斗 <daken@shimurahayatonoMacBook-Air.local>
2019-05-22 20:26:22 +0900

    [update]名前でログインできるように変更

--- Bookers2/db/schema.rb
+++ Bookers2/db/schema.rb
@@ -10,17 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_112847) do
+ActiveRecord::Schema.define(version: 2019_05_21_090835) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "homes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
